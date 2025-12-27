8707d95a2ab7faeb019b8a78cb21adffc66bf19f
Prarthana-cr <prarthanacr@gmail.com>
2025-12-27 12:24:41 +0530

    Add files via upload

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,15 +1,23 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
 
 city = input("Enter city name: ").strip()
 
-api_key = "4dc73d599288a4506394bf4927de7022"
+api_key = os.getenv("WEATHER_API_KEY")
+
+if not api_key:
+    print("API key not found! Please add it to the .env file.")
+    exit()
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.json() 
+    data = response.json()
     temp = data['main']['temp']
     condition = data['weather'][0]['description']
     print(f"Weather in {city}:")
